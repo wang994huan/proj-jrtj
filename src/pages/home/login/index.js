5962275eb8ae866ea6e9355e065f74b9f0af57ef
wang994huan <775093289@qq.com>
2019-05-26 22:42:53 +0800

    购物车未实现

--- src/pages/home/login/index.js
+++ src/pages/home/login/index.js
@@ -4,8 +4,23 @@ require(["../../../static/conf/config.js"], function() {
 			let data = {
 				username: $(".username").val(),
 				password: $(".password").val(),
+				login : true
 			};
-			console.log(data);
+			var array = [];
+			let pass = true;
+			if (localStorage.getItem("yonghu")) {
+				array = JSON.parse(localStorage.getItem("yonghu"));
+				for (var i = 0; i < array.length; i++) {
+					if (array[i].username == data.username) {
+						pass = false;
+						alert("用户名或者密码错误");
+					} 
+				}
+			}
+			if(pass){
+				array.push(data);
+			}
+			window.localStorage.setItem("yonghu", JSON.stringify(array));
 		})
 	});
 });
